070ff93392abfacd1a962eee30d4cd352c9fcf61
tyler <tylermarcus14@gmail.com>
2019-07-09 08:39:07 -0400

    updated files

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// Start with an array that hold 10 object to coincide with the 10 questions 
+//each object will have a key value pair of
+//name: (a string of the person name)
+// photo (with a link)
+// scores array (10 strings to coincide with the answers options)
+// ===============================================================================
+
+
+var friends = [
+  
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+
